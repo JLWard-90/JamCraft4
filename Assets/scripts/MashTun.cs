2d277fb354e717550e3fc1284d95cfeebcfdf53f
JLWard-90 <53184853+JLWard-90@users.noreply.github.com>
2019-11-30 09:25:52 +0100

    Created MashTun script

---
+++ Assets/scripts/MashTun.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MashTun : MonoBehaviour
+{
+    public void mashing = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnTimeStepForward()
+    {
+
+    }
+}
