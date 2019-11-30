d8598053f97f1188de0816710cd700d21ed279be
JLWard-90 <53184853+JLWard-90@users.noreply.github.com>
2019-11-30 16:17:29 +0100

    Added Fermenter
    Added Fermenter.cs
    
    updated Fermenter.cs

---
+++ Assets/scripts/Fermenter.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fermenter : MonoBehaviour
+{
+    public bool fermenting = false;
+    public bool readyToBottle = false;
+    public bool empty = true;
+    public Recipe recipe;
+    TimeController timeController;
+    int dayLength;
+    float gravityDropPerTimeStep;
+    public float currentGravity;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        timeController = GameObject.Find("GameController").GetComponent<TimeController>();
+        dayLength = timeController.gameTimeDayLength;
+    }
+    public void OnTransferWortIn(Recipe recipeIn)
+    {
+        recipe = recipeIn;
+        Yeast yeast = GameObject.Find("CompanyController").GetComponent<CompanyController>().inventory.availableYeasts[recipe.yeastIndex];
+        gravityDropPerTimeStep = (yeast.speed / (float)dayLength);
+        currentGravity = recipe.startingGravity;
+        fermenting = true;
+    }
+
+    public void OnTimeStepForward()
+    {
+        if(fermenting)
+        {
+            currentGravity -= gravityDropPerTimeStep;
+            if (currentGravity <= recipe.finalGravity)
+            {
+                currentGravity = recipe.finalGravity;
+                fermenting = false;
+                readyToBottle = true;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
